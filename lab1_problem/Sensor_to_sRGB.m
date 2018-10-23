f135ebee517ee582df439a691230b8af5f74c9d6
Adrian Mai <quocanh010@gmail.com>
2018-10-23 07:00:37 -0700

    Almost done

--- lab1_problem/Sensor_to_sRGB.m
+++ lab1_problem/Sensor_to_sRGB.m
@@ -4,7 +4,7 @@
 %Linearized Sensor image
 linearized_v = min(max(0, (cfa - info.black) / (info.white - info.black)), 1)
 
-%Demosaic
+%Demosaic(basically padding the RGB color so that there are 3 chanels)
 img = uint8(linearized_v*255)
 Demosaic = demosaic(img, info.bayer_type)
 %Normalize demosaic
@@ -29,14 +29,26 @@ color_24 = im2double(color_24)
 color_24 = color_24 .^ (2.2)
 %Get Device colors by extracting patches info and average them
 device_color = zeros([24,3])
-for i = 1:24
-    x_s = max(patch_24(i, 1), patch_24(i + 3, 1)) % horizontal staring point of the rectagular region
-    x_e = x_s + min(patch_24(i+1, 1) - patch_24(i, 1), patch_24(i+2, 1) - patch_24(i + 3, 1)) % horiontal end point of the rectangular region
-    y_s = max(patch_24(i, 2), patch_24(i + 1, 2)) % vertical start of the retagular
-    y_e = y_s + min(patch_24(i+3, 2) - patch_24(i, 2), patch_24(i+2, 2) - patch_24(i+1, 2))  % vertical end of teh rectangular
-    patch_i = Demosaic(y_s : y_e, x_s : x_e, :)
-    device_color(i, 1) = mean2(patch_i(:,:,1))
-    device_color(i, 2) = mean2(patch_i(:,:,2))
-    device_color(i, 3) = mean2(patch_i(:,:,3))
+for i = 1:1:24
+    x_s = max(patch_24((i-1) * 4 + 1, 1), patch_24((i-1) * 4 + 1 + 3, 1)) % horizontal staring point of the rectagular region
+    x_e = x_s + min(patch_24((i-1) * 4 + 1+1, 1) - patch_24((i-1) * 4 + 1, 1), patch_24((i-1) * 4 + 1+2, 1) - patch_24((i-1) * 4 + 1 + 3, 1)) % horiontal end point of the rectangular region
+    y_s = max(patch_24((i-1) * 4 + 1, 2), patch_24((i-1) * 4 + 1 + 1, 2)) % vertical start of the retagular
+    y_e = y_s + min(patch_24((i-1) * 4 + 1 + 3, 2) - patch_24((i-1) * 4 + 1, 2), patch_24((i-1) * 4 + 1 + 2, 2) - patch_24((i-1) * 4 + 1+1, 2))  % vertical end of teh rectangular
+    device_color(i, :) = mean(Demosaic(y_s : y_e, x_s : x_e, :), [1 2])
 end
+
+% use Penseudoinverse 
+M = pinv(device_color) * color_24
+% Fatten the original matrix
+Demosaic = reshape(Demosaic, [size(Demosaic,1) * size(Demosaic,2), 3])
+%Time the conversion matrix to get sRGB
+sRGB_img = Demosaic * M
+%Reshape to creat image
+sRGB_img = reshape(real(sRGB_img), [size(cfa,1), size(cfa,2), 3])
+%Gamma correction
+sRGB_img = lin2rgb(sRGB_img,'OutputType','double');
+%Reshape to creat image
+sRGB_img = reshape(real(sRGB_img), [size(cfa,1), size(cfa,2), 3])
+imshow(sRGB_img)
+
         
