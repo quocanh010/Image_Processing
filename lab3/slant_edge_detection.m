0517467815ed8ea8ad587b9bb36565fce024dba4
quocanh010 <quocanh010@gmail.com>
2018-11-20 14:57:31 -0800

    Done

--- lab3/slant_edge_detection.m
+++ lab3/slant_edge_detection.m
@@ -1,6 +1,6 @@
 
 img = imread('slant_edge.tif');
-img = im2double(img);
+%img = im2double(img);
 m = mean(img, 'all');
 x = zeros(64, 1);
 y = zeros(64, 1);
@@ -16,21 +16,39 @@ for yi = 1:64
     y(yi,1) = yi;
 end
 
-p = polyfit(y,x,1)
-f = y * p(1) + p(2)
+p = polyfit(y,x,1);
+f = y * p(1) + p(2);
 f = int8(f)
+xi = 0:63;
 figure;
 imshow(img);
 hold on;
 plot(f,y,'Color', 'r', 'LineStyle', '-', 'LineWidth',3);
 hold off;
 xf = p(1) * y;
-x_t = round(4 * (x - xf));
+x_t = round(4 * (xi' - (xf)));
 %Binning
-bin_v = x_t
-for i = 1:64
-    bin_v = bin_v + img(i,i);
+bin_v = zeros(284, 1);
+bin_c = zeros(284, 1);
+
+for y_i = 1:64
+    for x_i = 1:64
+        x_c = round(4 * (x_i - p(1) * y_i));
+        bin_v(x_c) = double(bin_v(x_c)) + double(img( y_i, x_i));
+        bin_c(x_c) = bin_c(x_c) + 1;
+    end
 end
-bin_c = bin_v + 1;
-bin_avg = bin_v / bin_c;
-lin = diff(bin_avg,1 , 2)/2;
+
+bin_avg = bin_v ./ bin_c;
+ESF = bin_avg(4:259);
+lin = diff(ESF, 1 , 1)/2;
+plot(ESF);
+plot(lin);
+a = fft(lin);
+SRF = (abs(a) - min(abs(a)))  / (max(abs(a)) - min(abs(a)));
+x_a = 1:255;
+x_a = (x_a - 1) * 4/ 254;
+
+plot(x_a(1:127), SRF(1:127));
+%plot(a);
+%plot([0:4], abs(a));
